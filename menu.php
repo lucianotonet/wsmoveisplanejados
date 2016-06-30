bb48d32fa04866d7b529d08387d7385170a1c780
Luciano <tonetlds@gmail.com>
2016-06-30 15:57:12 -0300

    Small bug fixes

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
 		<div class="container">
 			<div class="header-main">
 				<div class="logo">
-					<a href="<?php echo bloginfo('url' ); ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/logo1.png" alt=""></a>
+					<a href="<?php echo bloginfo('url'); ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/logo1.png" alt=""></a>
 				</div>
 				<div class="top-nav">
 					<span class="menu"> <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icon.png" alt=""/></span>
